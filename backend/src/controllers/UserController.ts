0a606ce4415b0d0af3cc0aa755b81264678353ec
Felipe Molinari <felipemolinari874@gmail.com>
2020-09-10 17:17:26 -0300

    Setup node, ts config and paths

---
+++ backend/src/controllers/UserController.ts
@@ -0,0 +1,8 @@
+import User from "@models/User";
+
+export default class UserController{
+  test(){
+    const user = new User();
+    return user;
+  }
+}
---
+++ backend/src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+import UserController from '@controllers/UserController'
+const app = express();
+
+app.get('/', (_,res)=>{
+  console.log(UserController)
+  return res.json({message: 'Hello world'})
+})
+app.listen(3333);
